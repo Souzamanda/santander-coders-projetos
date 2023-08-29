7505ff802285636fdad6438537befa89ca9685b9
Amanda Puttomatti <acsputtomatti@gmail.com>
2023-08-28 22:08:57 -0300

    Aula 28/08/2023 - Generics

--- src/POO2/generics/Generics.java
+++ src/POO2/generics/Generics.java
@@ -4,14 +4,16 @@ public class Generics {
     public static void main(String[] args) {
         String a = "a";
         String b = "b";
-        System.out.println("Maior entre a e b: " + maior(a, b));
+        String maiorAlfabeticamente = (String) maior(a, b);
+        System.out.printf("Maior entre %s e %s: %s%n", a, b, maiorAlfabeticamente);
+
+        Integer cinco = 5;
+        Integer tres = 3;
+        Integer maiorNumericamente = (Integer) maior(cinco, tres);
+        System.out.printf("Maior entre %s e %s: %s%n", cinco, tres, maiorNumericamente);
     }
 
-    private static String maior (String primeira, String segunda) {
-        if (primeira.compareTo(segunda) == 0) {
-            return primeira;
-        } else {
-            return segunda;
-        }
+    private static Comparable maior (Comparable primeira, Comparable segunda) {
+        return primeira.compareTo(segunda) > 0 ? primeira : segunda;
     }
 }
