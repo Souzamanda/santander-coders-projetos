af0bd34024a008e142d1cd24c371fe89078838e4
Amanda Puttomatti <acsputtomatti@gmail.com>
2023-09-01 19:49:56 -0300

    Aula 01/09/2023 — Adicionando Generics no exercicio "sorteador"

--- src/POO2/sorteador/Grupo.java
+++ src/POO2/sorteador/Grupo.java
@@ -2,17 +2,17 @@ package POO2.sorteador;
 
 import java.util.List;
 
-public class Grupo {
-    private final List<String> elementosDoGrupo;
+public class Grupo<T> {
+    private final List<T> elementosDoGrupo;
     private final int indice;
 
-    public Grupo(List<String> elementosDoGrupo, int indice) {
+    public Grupo(List<T> elementosDoGrupo, int indice) {
         this.elementosDoGrupo = elementosDoGrupo;
         this.indice = indice;
     }
 
 
-    public void adicionarNoGrupo (String novo) {
+    public void adicionarNoGrupo (T novo) {
         elementosDoGrupo.add(novo);
     }
 
